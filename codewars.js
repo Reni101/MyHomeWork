8c31f1f18ec303d53f7a56a9634501d318a21cb4
Максим Дмитриев <96905902+Reni101@users.noreply.github.com>
2022-03-20 10:23:23 +0300

    Codewars Switch it Up! alternative solution

--- codewars.js
+++ codewars.js
@@ -1,7 +1,6 @@
 let numbers = ["Zero", "One", "Two", "Three","Four", 
 	      "Five", "Six", "Seven", "Eight", "Nine"];
-var result;			
-	
+
 function switchItUp(val,result ) {
 	for(var i = 0; i < val.length; i++){
 		if (result == i){
