a3c18704563b80c3b96030902e96f55ffb8ff52b
Максим Дмитриев <96905902+Reni101@users.noreply.github.com>
2022-03-20 10:19:26 +0300

    Codewars  Switch it Up!   alternative solution
    https://www.codewars.com/kata/5808dcb8f0ed42ae34000031

---
+++ codewars.js
@@ -0,0 +1,14 @@
+let numbers = ["Zero", "One", "Two", "Three","Four", 
+				"Five", "Six", "Seven", "Eight", "Nine"];
+var result;			
+	
+function switchItUp(val,result ) {
+	for(var i = 0; i < val.length; i++){
+		if (result == i){
+			return console.log(numbers[i])
+		}
+	}
+}
+
+switchItUp(numbers, 9);
+
