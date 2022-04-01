141e62bb187e6493c186699130d450c22a8513a8
Максим Дмитриев <96905902+Reni101@users.noreply.github.com>
2022-04-01 17:26:27 +0300

    Update js.js

--- Объекты/js.js
+++ Объекты/js.js
@@ -16,7 +16,7 @@ let user = {
   
   let hi = user.hi.bind(user);
   hi(); 
- */
+
 
   function User(name, age) {
 	this.name = name;
@@ -26,4 +26,68 @@ let user = {
   
   let user = new User("Вася", 15);
 
-  console.log(user)
+  console.log(user) 
+
+
+*/
+/* 
+  function BigUser() {
+	this.name = "Вася";
+	return { name: "Godzilla" };  //Другими словами, return с объектом возвращает объект,
+	  					 // в любом другом случае конструктор вернёт this.
+  }
+  
+  let biguser =  new BigUser();
+
+  
+  function SmallUser() {
+	this.name = "Вася";
+  	return 15;
+  }
+console.log( new SmallUser());   */
+
+
+/*   function User(name) {
+	this.name = name;
+  
+	this.sayHi = function() {
+	  alert( "Меня зовут: " + this.name );
+	};
+  }
+  
+  let vasya = new User("Максим");
+  
+  vasya.sayHi();  */
+
+/*   let obj = {};
+  function A () {
+	return obj;
+  }
+  function B (name) {
+	return obj;;
+  }
+	let a = new A;
+	let b = new B;
+console.log(a === b); // true, я  в шоке
+ */
+
+function Calculator() {
+	this.read = function() {
+		this.a = +prompt('Введите первое число');
+		this.b = +prompt('Введите первое число');
+	};
+
+	this.sum = function() {
+		return this.a + this.b;
+	};
+
+	this.mul = function() {
+		return this.a * this.b;
+	};
+}
+
+let calculator = new Calculator();
+calculator.read();
+
+alert( "Sum = " + calculator.sum() );
+alert( "Mul = " + calculator.mul() );
