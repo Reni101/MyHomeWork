e80abfa84e5bc58498f4ba246c346c8d3377bb62
Максим Дмитриев <96905902+Reni101@users.noreply.github.com>
2022-04-03 13:29:10 +0300

    Update js.js

--- Типы данных/js.js
+++ Типы данных/js.js
@@ -1,6 +1,6 @@
-/* let str = "Привет";
+/*  let str = "Привет";
 alert( str.toUpperCase() );
- */
+
 
 
 // let str = "Привет"; примитивы не являются объектами
@@ -48,3 +48,66 @@ alert( parseFloat('12.3.4') ); // 12.3, произойдёт остановка 
 Math.random()
 Возвращает псевдослучайное число в диапазоне от 0 (включительно) до 1 (но не включая 1)
 alert( Math.random() );
+
+Math.max(a, b, c...) / Math.min(a, b, c...)
+Возвращает наибольшее/наименьшее число из перечисленных аргументов.
+
+alert( Math.max(3, 5, -10, 0, 1) ); // 5
+
+Math.pow(n, power)
+Возвращает число n, возведённое в степень power
+alert( Math.pow(2, 10) );  2 в степени 10 = 1024 
+
+
+let a = +prompt("Введиет превое чилсо");
+let b = +prompt("Введиет второе чилсо");
+
+console.log( a + b);
+
+function readNumber() {
+	let num;
+	do {
+		num = +prompt("Введите число");
+	} 
+	while (!isFinite(num));
+
+	if (num === null || num === "") {
+		return null;
+	}
+	return +num;
+}
+
+console.log(readNumber());
+
+
+let i = 0;
+while (i != 10) { // i < 10
+  i += 0.2;
+}
+/таму шо i не станет 10
+
+*/
+
+function random(min, max) {
+	return Math.floor(Math.random() * (max + 1 - min) + min);
+	 
+}
+console.log(random(2,6))
+
+/*
+function randomInteger(min, max) {
+	// получить случайное число от (min-0.5) до (max+0.5)
+	let rand = min - 0.5 + Math.random() * (max - min + 1);
+	return Math.round(rand);
+  }
+alert( randomInteger(1, 3) );
+
+
+  function randomInteger(min, max) {
+	// случайное число от min до (max+1)
+	let rand = min + Math.random() * (max + 1 - min);
+	return Math.floor(rand);
+  }
+alert( randomInteger(1, 3) );
+
+*/
