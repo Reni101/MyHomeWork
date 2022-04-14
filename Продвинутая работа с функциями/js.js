9266ae9c39fa3979612d81935a0681baef37ad8e
Максим Дмитриев <96905902+Reni101@users.noreply.github.com>
2022-04-14 22:36:45 +0300

    Update js.js

--- Продвинутая работа с функциями/js.js
+++ Продвинутая работа с функциями/js.js
@@ -26,7 +26,7 @@ console.log(pow(2,3) );
 
 console.log(pow(2, 3)) */
 
-
+/* 
 let company = {
     sales: [{
         name: 'John',
@@ -67,7 +67,74 @@ function sumSalaries(department) {
 }
 
 console.log(sumSalaries(company));
+ */
 
+/* function sumTo(n) {
+    let x = 0;
+    for (let i = 1; i <= n; i++) {
+        x += i;
+    } return x;
+} */
+
+/* function sumTo(n) {
+    if (n === 1) {
+        return n;
+    } else {
+        return n + sumTo(n - 1);
+    };
+}
+
+console.log(sumTo(100));
+
+ */
+
+/* 
+function factorial(n) {
+    if (n === 1) {
+        return n
+    } else {
+        return n * factorial(n - 1);
+    };
+}
+console.log(factorial(3)) */
+
+/*
+let list = {
+    value: 1,
+    next: {
+        value: 2,
+        next: {
+            value: 3,
+            next: {
+                value: 4,
+                next: null
+            }
+        }
+    }
+};
+
+ function printList(list) {
+    let x = list;
+    while (x) {
+        console.log(x.value);
+        x = x.next;
+    }
+
+}
+
+printList(list); 
+
+function printList(x){
+    console.log(x.value);
+    if(x.next) {
+        printList(x.next);
+    }
+}
+
+printList(list);
+
+*/
+/* 
 function sumAll(...args) {
     let sum = 0;
     for(let arg of args) {
@@ -77,3 +144,13 @@ function sumAll(...args) {
 }
 
 console.log(sumAll(3,3,3,3,));
+
+
+function showName(firstName, lastName, ...titles){
+    console.log(firstName +" " + lastName);
+    console.log(titles)
+};
+
+showName("Юлий", "Цезарь", "Консул", "Император"); */
+
+console.log(Math.max(3, 5, 6))
